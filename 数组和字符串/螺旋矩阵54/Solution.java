fbc6091514ae08b058a0c4bc29b742413bdd2bcf
h00427535 <hezilong3@huawei.com>
2019-07-01 16:37:16 +0800

    add another solution

--- 数组和字符串/螺旋矩阵54/Solution.java
+++ 数组和字符串/螺旋矩阵54/Solution.java
@@ -29,9 +29,12 @@ class Solution {
 
     public static void main(String[] args) {
         int[][] input = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        System.out.println(new Solution().spiralOrder(input));
+        System.out.println(new Solution().spiralOrder2(input));
     }
 
+    /**
+     * 找规律，先从左到右，遇到边界或者已经访问，就按照顺时针改变方向。
+     */
     public List<Integer> spiralOrder(int[][] matrix) {
         if (matrix.length == 0) {
             return new ArrayList<>();
@@ -59,6 +62,39 @@ class Solution {
                 r = cr;
                 c = cc;
             }
+        }
+        return ret;
+    }
+
+    /**
+     * 逐层遍历
+     */
+    public List<Integer> spiralOrder2(int[][] matrix) {
+        List<Integer> ret = new ArrayList<>();
+        if (matrix.length == 0) {
+            return ret;
+        }
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int i = c1; i <= c2; i++) {
+                ret.add(matrix[r1][i]);
+            }
+            for (int i = r1 + 1; i <= r2; i++) {
+                ret.add(matrix[i][c2]);
+            }
+            if (r1 < r2 && c1 < c2) {
+                for (int i = c2 - 1; i > c1; i--) {
+                    ret.add(matrix[r2][i]);
+                }
+                for (int i = r2; i > r1; i--) {
+                    ret.add(matrix[i][c1]);
+                }
+            }
+            r1++;
+            r2--;
+            c2--;
+            c1++;
         }
         return ret;
     }
