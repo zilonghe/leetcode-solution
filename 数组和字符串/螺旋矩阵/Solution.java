9e8ad1dcc4578e9001f06c6e87807ffefce37de0
h00427535 <hezilong3@huawei.com>
2019-07-01 11:07:24 +0800

    new Solution

--- 数组和字符串/螺旋矩阵/Solution.java
+++ 数组和字符串/螺旋矩阵/Solution.java
@@ -1,8 +1,7 @@
 package 数组和字符串.螺旋矩阵;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
@@ -27,10 +26,40 @@ import java.util.Set;
  * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
  */
 class Solution {
-    private Set<Integer> visitedRow = new HashSet<>();
-    private Set<Integer> visitedCol = new HashSet<>();
+
+    public static void main(String[] args) {
+        int[][] input = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        System.out.println(new Solution().spiralOrder(input));
+    }
 
     public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) {
+            return new ArrayList<>();
+        }
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
 
+        List<Integer> ret = new ArrayList<>();
+        int r = 0;
+        int c = 0;
+        int di = 0;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean[][] visited = new boolean[row][col];
+        for (int i = 0; i < row * col; i++) {
+            ret.add(matrix[r][c]);
+            visited[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (cr >= row || cr < 0 || cc < 0 || cc >= col || visited[cr][cc]) {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            } else {
+                r = cr;
+                c = cc;
+            }
+        }
+        return ret;
     }
 }
