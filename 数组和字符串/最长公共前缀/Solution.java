61d9c59c69035a6adcd6a68ecf7cae1288d09688
h00427535 <hezilong3@huawei.com>
2019-07-02 17:42:55 +0800

    new Solution

---
+++ 数组和字符串/最长公共前缀/Solution.java
@@ -0,0 +1,25 @@
+package 数组和字符串.最长公共前缀;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
