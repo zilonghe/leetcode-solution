85d3f0a71f2041b6af736e9ab47a2c64cbc16de9
zilonghe <495309675@qq.com>
2019-07-10 13:02:07 +0800

    new solution

---
+++ 数组和字符串/最长公共前缀14/Solution.java
@@ -0,0 +1,37 @@
+package 数组和字符串.最长公共前缀14;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ */
+class Solution {
+    public static void main(String[] args) {
+        String[] input = {"leetcode", "leet"};
+        System.out.println(new Solution().longestCommonPrefix(input));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
