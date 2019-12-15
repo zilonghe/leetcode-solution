01fe72ae657c8e7b9679ff62ef4220a2ffdc2495
zilonghe <495309675@qq.com>
2019-12-15 23:00:46 +0800

    new solution

---
+++ 链表/奇偶连表326/Solution.java
@@ -0,0 +1,59 @@
+package 链表.奇偶连表;
+
+/**
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ *
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 1->3->5->2->4->NULL
+ * 示例 2:
+ *
+ * 输入: 2->1->3->5->6->4->7->NULL
+ * 输出: 2->3->6->7->1->5->4->NULL
+ * 说明:
+ *
+ * 应当保持奇数节点和偶数节点的相对顺序。
+ * 链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        // 如果是空或者只有一个节点，则直接返回
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode oddHead = head;
+        ListNode evenHead = head.next;
+        ListNode oddTail = oddHead;
+        ListNode evenTail = evenHead;
+
+        head = evenHead.next;
+        int cnt = 0;
+
+        while(head != null) {
+            cnt++;
+            if (cnt % 2 == 0) {
+                evenTail.next = head;
+                evenTail = evenTail.next;
+            } else {
+                oddTail.next = head;
+                oddTail = oddTail.next;
+            }
+            head = head.next;
+        }
+        oddTail.next = evenHead;
+        evenTail.next = null;
+        return oddHead;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
